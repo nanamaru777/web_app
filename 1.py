d843b89190199f537e08c6cc80fbb5ae3869f75f
nanamaru777 <rightjoin@yahoo.co.jp>
2024-07-23 22:36:35 +0900

    最初のコミット

---
+++ 1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("# 便利な Excel API を使ってみよう")
+st.write("Microsoft Excelの機能をプログラムから操作するためのAPI（アプリケーションプログラミングインターフェース）です。これにより、開発者はプログラムやスクリプトを通じてExcelワークシートを操作し、データの入力、編集、解析、グラフの作成などを自動化することができます。")
+
+st.write("詳しくは、ExcelAPIの公式ドキュメントをご覧ください：[ExcelAPI公式ドキュメント](https://excelapi.org/docs/)")
